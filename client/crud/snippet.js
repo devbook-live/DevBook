dddabb7cdecacd318fcb18f2990bf313dc6c0540
Ben Ellentuck <ellentuckben@gmail.com>
2018-05-07 16:51:08 -0400

    snippetById working

--- client/crud/snippet.js
+++ client/crud/snippet.js
@@ -1,7 +1,7 @@
 import db from '../../firebase/initFirebase';
 
 // create a snippet
-export const createSnippet = (
+const createSnippet = (
   text,
   language,
   doc, // foreign key for the document that includes this snippet
@@ -24,12 +24,39 @@ export const createSnippet = (
     });
 };
 
-// find a single snippet
+// find a single snippet (by id)
+const snippetById = (id) => {
+  return db.collection('snippets').doc(id).get()
+    .then((snippet) => {
+      if (snippet.exists) {
+        console.log('Snippet data:', snippet.data());
+        return snippet;
+      }
+      // else, snippet.data() will be undefined
+      console.log('No such snippet exists.');
+      return null;
+    })
+    .catch((error) => {
+      console.log('Error getting snippet:', error);
+    });
+};
 
 // find all snippets for a particular user
 
+// find all snippets for a particular group
+
 // find all snippets for a particular document
+
+// find all snippets for a particular language
+
+// find all snippets ever (should be limited to admins only!)
 
 // update a snippet
 
 // delete a snippet
+
+
+module.exports = {
+  createSnippet,
+  snippetById,
+};
