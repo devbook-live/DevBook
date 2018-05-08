fed629473e6f526c4f3d9efaedd8279537e03d8a
Fred Ma <fma1@terpmail.umd.edu>
2018-05-08 18:35:54 -0400

    Added comments

--- client/crud/group.spec.js
+++ client/crud/group.spec.js
@@ -1,19 +1,24 @@
-/* global describe before after it */
+/* global describe before it */
 /* eslint-disable global-require */
 /* eslint-disable max-len */
 /* eslint-disable new-cap */
 
+// Overriding dependencies
 const proxyquire = require('proxyquire').noCallThru();
+// Firebase mocking
 const firebasemock = require('firebase-mock');
 
+// Creates a mock for authentication
 const mockauth = new firebasemock.MockFirebase();
+
+// Creates a mock for firestore
 let mockfirestore = new firebasemock.MockFirestore();
-const mocksdk = firebasemock.MockFirebaseSdk(null, () => {
-  return mockauth;
-}, () => {
-  return mockfirestore;
-});
-const mockapp = mocksdk.initializeApp();
+
+// function MockFirebaseSdk(createDatabase, createAuth, createFirestore, createStorage, createMessaging)
+// so if you specify createFirestore parameter, then in the returned object from MockFirebaseSdk,
+// value for the key "firestore" will be what you passed in as createFirestore
+// We aren't using this at the moment though
+const mocksdk = firebasemock.MockFirebaseSdk(null, () => mockauth, () => mockfirestore);
 
 const chai = require('chai');
 
@@ -24,22 +29,29 @@ describe('Cloud Functions', () => {
 
   describe('addGroup', () => {
     before(() => {
+      // create a new mock for firestore before we run stuff
       mockfirestore = new firebasemock.MockFirestore();
+      // causes promises to immediately resolve
       mockfirestore.autoFlush();
+      // So group.js is requiring '../../firebase/initFirebase', right?
+      // What this is doing is overriding that require with our mockfirestore
       myFunctions = proxyquire('./group.js', {
         '../../firebase/initFirebase': mockfirestore,
       });
     });
 
     it('add a Group 5 group', (done) => {
+      // same thing as db.collection('groups') in groups.js
       const groupsRef = mockfirestore.collection('groups');
 
       myFunctions.addGroup('Group 5', { snippets: { 1: true, 2: true }, users: { 1: true, 2: true, 3: true } })
         .then(() => {
+          // getData() returns { 'Group 5': { snippets: { 1: true, 2: true }, users: { 1: true, 2: true, 3: true } } }
           const group5 = groupsRef.getData()['Group 5'];
-          assert(group5.users && group5.users[1]);
-          done();
-        });
+          // So we just check that that there's a users key for 'Group 5' and then there's a '1' key for users in 'Group 5'
+          assert(Object.prototype.hasOwnProperty.call(group5, 'users') && Object.prototype.hasOwnProperty.call(group5.users, '1'));
+        })
+        .finally(done);
     });
   });
 });
