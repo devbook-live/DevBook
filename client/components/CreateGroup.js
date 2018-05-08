f5bf9963d2df263c90275e470ce55cd6b9f97b25
Jang Won Ko <jangwon415@gmail.com>
2018-05-08 17:44:27 -0400

    alert if existing user is being added to a group

--- client/components/CreateGroup.js
+++ client/components/CreateGroup.js
@@ -32,7 +32,15 @@ class CreateGroup extends Component {
   }
 
   handleAddUser() {
-    this.setState({ users: [...this.state.users, this.state.chosenUser] });
+    const userIds = [];
+    this.state.users.forEach(user => userIds.push(user.id));
+    if (userIds.includes(this.state.chosenUser.id)){
+      alert(`${this.state.chosenUser.name} is already selected!`);
+    }
+    else {
+      this.setState({ users: [...this.state.users, this.state.chosenUser] });
+    }
+    this.refs['autocomplete'].setState({searchText: ''})
   }
 
   handleRequestDelete(key) {
@@ -42,7 +50,8 @@ class CreateGroup extends Component {
     this.setState({ users: this.users });
   }
 
-  handleCreateGroup() {
+  handleCreateGroup(event) {
+    event.preventDefault();
     const name = this.state.name;
     const users = {};
     this.state.users.map(user => {
@@ -99,6 +108,7 @@ class CreateGroup extends Component {
           {this.state.users.map(this.renderChip, this)}
         </div>
         <AutoComplete
+          ref={`autocomplete`}
           floatingLabelText="User Name"
           filter={AutoComplete.fuzzyFilter}
           dataSource={fakeUsers}
