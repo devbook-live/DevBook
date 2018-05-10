c787f43533ca7c7c029488f13dc30e5e1b52569a
Ben Ellentuck <ellentuckben@gmail.com>
2018-05-10 12:02:13 -0400

    added to components index file so we know what components to build

--- client/components/index.js
+++ client/components/index.js
@@ -3,8 +3,17 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
+
+
 export { default as Navbar } from './navbar';
-export { default as UserHome } from './user-home';
-export { Login, Signup } from './auth-form';
 export { default as CodeSnippet } from './Snippet';
+
+// components for which there are routes:
+export { Login, Signup } from './auth-form';
+export { default as AllGroups } from './AllGroups';
+export { default as SingleGroup } from './SingleGroup';
+export { default as AllUsers } from './AllUsers';
+export { default as SingleUser } from './SingleUser';
+export { default as AllNotebooks } from './AllNotebooks';
+export { default as SingleNotebook } from './SingleNotebook';
 export { default as CreateGroup } from './CreateGroup';
