22124435334c512550aff82661cfd39105383f14
Jang Won Ko <jangwon415@gmail.com>
2018-05-11 12:16:30 -0400

    deleted comments in SingleGroup.js

--- client/components/SingleGroup.js
+++ client/components/SingleGroup.js
@@ -9,26 +9,6 @@ import { Card } from 'material-ui/Card';
 import UsersList from './UsersList';
 import DocsList from './DocsList';
 
-
-// const styles = theme => ({
-//   container: {
-//     display: 'grid',
-//     gridTemplateColumns: 'repeat(12, 1fr)',
-//     gridGap: `${theme.spacing.unit * 3}px`,
-//   },
-//   paper: {
-//     padding: theme.spacing.unit,
-//     textAlign: 'center',
-//     color: theme.palette.text.secondary,
-//     whiteSpace: 'nowrap',
-//     marginBottom: theme.spacing.unit,
-//   },
-//   divider: {
-//     margin: `${theme.spacing.unit * 2}px 0`,
-//   },
-// });
-
-
 class SingleGroup extends Component {
   render() {
     const { groupId } = this.props.match.params;
