fa0be6df593fcb48eb0370f59a21f0a8a731729f
Jang Won Ko <jangwon415@gmail.com>
2018-05-11 12:11:21 -0400

    improved singleGroup page styling

--- client/components/SingleGroup.js
+++ client/components/SingleGroup.js
@@ -5,41 +5,41 @@ import Chip from 'material-ui/Chip';
 import Avatar from 'material-ui/Avatar';
 import RaisedButton from 'material-ui/RaisedButton';
 import { getGroupsByUserId, getGroupById, addGroup, updateGroup, deleteGroup } from '../crud/group';
-import Paper from 'material-ui/Paper';
+import { Card } from 'material-ui/Card';
 import UsersList from './UsersList';
 import DocsList from './DocsList';
 
 
-const styles = theme => ({
-  container: {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(12, 1fr)',
-    gridGap: `${theme.spacing.unit * 3}px`,
-  },
-  paper: {
-    padding: theme.spacing.unit,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    whiteSpace: 'nowrap',
-    marginBottom: theme.spacing.unit,
-  },
-  divider: {
-    margin: `${theme.spacing.unit * 2}px 0`,
-  },
-});
+// const styles = theme => ({
+//   container: {
+//     display: 'grid',
+//     gridTemplateColumns: 'repeat(12, 1fr)',
+//     gridGap: `${theme.spacing.unit * 3}px`,
+//   },
+//   paper: {
+//     padding: theme.spacing.unit,
+//     textAlign: 'center',
+//     color: theme.palette.text.secondary,
+//     whiteSpace: 'nowrap',
+//     marginBottom: theme.spacing.unit,
+//   },
+//   divider: {
+//     margin: `${theme.spacing.unit * 2}px 0`,
+//   },
+// });
 
 
 class SingleGroup extends Component {
   render() {
     const { groupId } = this.props.match.params;
     return (
-      <div>
-        <Paper className={styles.paper}>
+      <div className="singleGroupPage">
+        <Card className="usersList">
           <UsersList groupId={groupId}/>
-        </Paper>
-        <Paper className={styles.paper}>
+        </Card>
+        <Card className="docsList">
           <DocsList groupId={groupId}/>
-        </Paper>
+        </Card>
       </div>
     )
   }
