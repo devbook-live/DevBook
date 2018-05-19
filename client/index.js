12e6390c03a49934bbfba23e09aae1a1443993dc
Fred Ma <fma1@terpmail.umd.edu>
2018-05-19 17:17:02 -0400

    Removed most of stuff from boilermaker

--- client/index.js
+++ client/index.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 // local imports
 import history from './history';
-import store from './store';
 import App from './app';
 
-
-// establishes socket connection
-import './socket';
-
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <MuiThemeProvider>
-        <App />
-      </MuiThemeProvider>
-    </Router>
-  </Provider>,
+  <Router history={history}>
+    <MuiThemeProvider>
+      <App />
+    </MuiThemeProvider>
+  </Router>
   document.getElementById('app'),
 );
