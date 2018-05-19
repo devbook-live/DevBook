89de463b3a87152308b39802a88a46f1f44071fd
Ben Ellentuck <ellentuckben@gmail.com>
2018-05-19 19:00:58 -0400

    Fixed bug so that now, whatever page of the app you load, you will be automatically signed-in anonymously if you are not already signed in.

--- client/app.js
+++ client/app.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Navbar } from './components';
 import Routes from './routes';
+import { auth } from '../firebase/initFirebase';
 
-
-const App = () => (
-  <div>
-    <Navbar />
-    <Routes />
-  </div>
-);
+class App extends Component {
+  componentDidMount() {
+    if (!auth.currentUser) auth.signInAnonymously();
+  }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Routes />
+      </div>
+    );
+  }
+}
 
 export default App;
