eaa7521e1429fe7e54a1163833be52022858ff72
Fred Ma <fma1@terpmail.umd.edu>
2018-05-26 16:51:07 -0400

    Added simple React tests for structure of SingleNotebook

--- client/components/SingleNotebook.spec.js
+++ client/components/SingleNotebook.spec.js
@@ -4,15 +4,14 @@ global.XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
 const { expect } = require('chai');
 const React = require('react');
-const enzyme = require('enzyme');
+const { configure } = require('enzyme');
 const { render } = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 const { MemoryRouter } = require('react-router');
 const { auth } = require('../../firebase/initFirebase');
 const SingleNotebook = require('./SingleNotebook').default;
 
-const adapter = new Adapter();
-enzyme.configure({ adapter });
+configure({ adapter: new Adapter() });
 
 describe('SingleNotebook', () => {
   let singleNotebookWrapper;
@@ -26,29 +25,20 @@ describe('SingleNotebook', () => {
     singleNotebookWrapper = render(<MemoryRouter><SingleNotebook match={props} /></MemoryRouter>);
 
     auth.signInAnonymously()
-      .then(() => singleNotebookWrapper.setState({ users: [auth.currentUser.uid] }))
-      .then(() => console.warn('Hello'));
+      .then(() => singleNotebookWrapper.setState({ users: [auth.currentUser.uid] }));
   });
 
-  it('includes "Product Name" line as an h3', () => {
-    console.log(singleNotebookWrapper.find('NotebookMetadata').length);
-    expect([]).to.have.length(1);
-    // expect(singleNotebookWrapper.is('NotebookMetadata')).to.have.length(1);
+  it('includes NotebookMetadata', () => {
+    // So this gets the child with the NotebookMetadata selector and makes sure it
+    // is a NotebookMetadata element
+    expect(singleNotebookWrapper.children('NotebookMetadata').is('NotebookMetadata'));
   });
 
-  // it('includes "Product Name" line as an h3', () => {
-  //   expect(singleNotebookWrapper.find('h3').text()).to.equal('ProductName');
-  // });
+  it('includes NotebookFooter', () => {
+    expect(singleNotebookWrapper.children('NotebookFooter').is('NotebookFooter'));
+  });
 
-  // it('includes "Price" line as span', () => {
-  //   expect(singleNotebookWrapper.find('span').text()).to.equal('Price: $8');
-  // });
-
-  // it('includes "Image" line as img', () => {
-  //   expect(singleNotebookWrapper.find('img').html()).to.equal('<img src="http://www.agecomputer.org/images/computerhappy.png" height="100" width="100"/>');
-  // });
-
-  // it('includes "Description" as p', () => {
-  //   expect(singleNotebookWrapper.find('p').text()).to.equal('Product Description');
-  // });
+  it('includes CodeSnippet', () => {
+    expect(singleNotebookWrapper.children('CodeSnippet').is('CodeSnippet'));
+  });
 });
